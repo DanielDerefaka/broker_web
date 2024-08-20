ccd2e035f36269d9dd2ef8eefe59b26843d777b6
DanielDerefaka <bryancagedx@gmail.com>
2024-08-20 04:03:49 +0100

    done

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -845,3 +845,17 @@ export const handlesus = async (id: string) => {
     throw error;
   }
 };
+
+
+
+
+
+export const getAlTransactions = await client.transaction.findMany({
+  where: {
+    userId: authUser.id,
+  },
+  orderBy: {
+    createdAt: "desc", // Order by createdAt in descending order to get the latest deposits first
+  },
+  take: 4, // Limit the number of deposits to 4
+});
