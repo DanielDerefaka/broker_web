cffc1f00ee0c5547d74b346c0789bf73464776f3
DanielDerefaka <bryancagedx@gmail.com>
2024-08-20 05:30:06 +0100

    done

--- next.config.mjs
+++ next.config.mjs
@@ -2,30 +2,30 @@
 const nextConfig = {
   images: {
     domains: [
-      'uploadthing.com',
-      'utfs.io',
-      'img.clerk.com',
-      'subdomain',
-      'files.stripe.com',
+      "uploadthing.com",
+      "utfs.io",
+      "img.clerk.com",
+      "subdomain",
+      "files.stripe.com",
     ],
   },
   reactStrictMode: false,
-
 
   typescript: {
     // Ignoring TypeScript errors during build
     ignoreBuildErrors: true,
   },
 
-  eslint:{
-    ignoreDuringBuilds: true
-},
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push("@prisma/client");
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
-
-
-
-
-
-
