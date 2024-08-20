9553145d15bbaeab6d085ccc25c2dc1f78684129
DanielDerefaka <bryancagedx@gmail.com>
2024-08-20 05:10:50 +0100

    done again

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: [
-        'uploadthing.com',
-        'utfs.io',
-        'img.clerk.com',
-        'subdomain',
-        'files.stripe.com',
-        "dc.om"
-      ],
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: '*',
-          },
-          {
-            protocol: 'http',
-            hostname: '*',
-          },
-        ],
-      },
-      typescript: {
-        // Ignoring TypeScript errors during build
-        ignoreBuildErrors: true,
-      },
-  
-      eslint:{
-        ignoreDuringBuilds: true
-    },
-    reactStrictMode: true,
-  };
-  
-  export default nextConfig;
+  images: {
+    domains: [
+      'uploadthing.com',
+      'utfs.io',
+      'img.clerk.com',
+      'subdomain',
+      'files.stripe.com',
+    ],
+  },
+  reactStrictMode: false,
+};
+
+export default nextConfig;
---
+++ src/app/(main)/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import Dashpage from '@/components/dashboard/Dashpage'
+import Infobar from '@/components/Infobar'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <Infobar/>
+
+{/* <Dash/> */}
+<Dashpage/>
+    </div>
+  )
+}
+
+export default page
